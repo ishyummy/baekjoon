c75c3d058d2aaef4487ee299dedd0e42b92d166c
ishyummy <asiapjh@naver.com>
2021-10-18 23:57:08 +0900

    211018

--- 프로그래머스 K번째수.py
+++ 프로그래머스 K번째수.py
@@ -1,2 +1,7 @@
-a = [1, 2]
-a[2] = 3
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        sub = array[command[0]-1:command[1]]
+        sub.sort()
+        answer.append(sub[command[2]-1])
+    return answer
